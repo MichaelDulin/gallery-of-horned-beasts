28fcc74e7b30a6b3d77ec8fd34e84b4f9c745059
Michael Dulin <michaeldulin1211@gmail.com>
2023-03-01 12:44:28 -0800

    almost working

--- src/App.js
+++ src/App.js
@@ -6,20 +6,57 @@ import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Modal from 'react-bootstrap/Modal';
 import './App.css';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      beastName: ''
+      // beastImgURL: '',
+      // beastDescription: ''
+    }
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  handleOpenModal = (name) => {
+    this.setState({
+      showModal: true,
+      beastName: name
+    })
+  }
+
   render() {
     return (
       <>
         <Header />
+        <p onClick={this.handleOpenModal}></p>
         <Container>
           <Row>
-            <Main data={data} />
+            <Main data={data} handleOpenModal={this.handleOpenModal} />
           </Row>
         </Container>
         <Footer />
+        <Modal
+          show={this.state.showModal}
+          onHide={this.handleCloseModal}
+        >
+          <Modal.Header>
+            <Modal.Title>
+              {this.state.name}
+            </Modal.Title>
+          </Modal.Header>
+          {/* {this.state.imageURL}
+          {this.state.description} */}
+        </Modal>
       </>
     );
   }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -17,26 +17,30 @@ class HornedBeast extends React.Component {
     });
   }
 
+  handleHeaderClick = () => {
+    this.props.handleOpenModal(this.props.name)
+  }
+
   render() {
     return (
-        <Col style={{ margin: '1.5rem'}}>
-          <Card style={{minWidth: '18rem'}}>
-            <article className="hornedBeast" >
-              <h2>{this.props.name}</h2>
-              <p>❤️ {this.state.likes}</p>
-              <p onClick={this.handleLikes}>Like!</p>
-              <img
-                src={this.props.imageURL}
-                alt={this.props.keyword}
-                style={{maxHeight: '12rem',
-                maxWidth: '12rem',
-              }}
-              />
-              <p>{this.props.description}</p>
-              <p>Number of Horns: {this.props.horns}</p>
-            </article>
-          </Card>
-        </Col>
+      <Col style={{ margin: '1.5rem' }}>
+        <Card style={{ minWidth: '18rem' }}>
+          <article className="hornedBeast">
+            <h2>{this.props.name}</h2>
+            <p>❤️{this.state.likes}</p>
+            <p onClick={this.handleLikes}>Like!</p>
+            <img
+              src={this.props.imageURL}
+              alt={this.props.keyword}
+              onClick={this.props.handleHeaderClick}
+              style={{ maxHeight: '12rem', maxWidth: '12rem' }}
+            >{this.props.name}
+            </img>
+            <p>{this.props.description}</p>
+            <p>Number of Horns: {this.props.horns}</p>
+          </article>
+        </Card>
+      </Col>
     );
   }
 }
